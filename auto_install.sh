06994c4e1aab5642985ad89afd42a97c0dba5fde
Batwam <56710655+Batwam@users.noreply.github.com>
2023-04-08 00:53:37 +0800

    user list in menu

--- auto_install.sh
+++ auto_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-DEFAULT_INSTALL_DIR="$HOME/.local/share/gnome-shell/extensions/fastuserswitch@batwam.corp"
+extension="paneluserswitch@batwam.corp"
+DEFAULT_INSTALL_DIR="$HOME/.local/share/gnome-shell/extensions/$extension"
 
 if [ -d $DEFAULT_INSTALL_DIR ]; then
 	rm -rf $DEFAULT_INSTALL_DIR
@@ -10,6 +11,8 @@ cd "$(dirname "$0")/src"
 printf "\e[32mCopying extension files to target directory:\n\e[0m"
 cp -Rv ./* $DEFAULT_INSTALL_DIR
 
+cd $OLDPWD
+
 if [ $XDG_SESSION_TYPE = "x11" ]; then
 	printf "\n\e[32mAll files copied. \nReloading the gnome-shell (shortcut Alt + F2, r) to load the extension.\n\n\e[0m"
 	killall -3 gnome-shell
@@ -17,4 +20,8 @@ else
 	printf "\n\e[32mAll files copied. \nPlease log out and log back in again to load the extension.\n\n\e[0m"
 fi
 
-cd $OLDPWD
+journalctl --follow -o cat /usr/bin/gnome-shell GNOME_SHELL_EXTENSION_UUID=$extension
+
+
+
+
--- src/extension.js
+++ src/extension.js
@@ -5,11 +5,15 @@ const {Clutter,Gio,GLib,GObject,St,Gdm,AccountsService} = imports.gi;
 const Mainloop = imports.mainloop;
 const ExtensionUtils = imports.misc.extensionUtils;
 const CurrentExtension = ExtensionUtils.getCurrentExtension();
+const AuthPrompt = imports.gdm.authPrompt;
+
+const Gettext = imports.gettext.domain('paneluserswitch');
+const _ = Gettext.gettext;
 
 let indicator = null;
 
 function enable(){
-	indicator = new FastUserSwitch();
+	indicator = new PanelUserSwitch();
 }
 
 function disable(){
@@ -18,106 +22,97 @@ function disable(){
 	indicator = null;
 }
 
-var FastUserSwitch = GObject.registerClass(
-	{ GTypeName: 'FastUserSwitch' },
-class FastUserSwitch extends PanelMenu.Button {
+var PanelUserSwitch = GObject.registerClass(
+	{ GTypeName: 'PanelUserSwitch' },
+class PanelUserSwitch extends PanelMenu.Button {
 	_init(){
-		super._init(0.0,'FastUserSwitch',false);
+		super._init(0.0,'PanelUserSwitch',false);
 
-		// this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.fastuserswith'); //need to add schema to use
-
-		this.box = new St.BoxLayout({
-			x_align: Clutter.ActorAlign.FILL
-		});
+		this.box = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
 		this.add_child(this.box);
+		
+		let icon = new St.Icon({ icon_name: 'system-users-symbolic',
+								style_class: 'system-status-icon' });
+		this.box.add_child(icon);
 
-		this.label = new St.Label({
-			text: "test",
-			y_align: Clutter.ActorAlign.CENTER
-		});
-		this.box.add_child(this.label);
+log(Date().substring(16,24)+' PanelUserSwitch/src/extension.js: '+'---------------------------');
 
-		this._buildMenu();
-		this.connect('button-press-event',this._buildMenu.bind(this));
+		this._users = [];
+		this._items = [];
 
-		Main.panel.addToStatusArea('FastUserSwitch',this,0,'right'); //position,panel_side
-
-		this._refresh();
-	}
-
-	_buildMenu(){
-		this.menu.removeAll(); //start by deleting everything
-
-	  //settings shortcut:
-		this.menu.addAction(_('Settings'), () => ExtensionUtils.openPrefs());
-	}
-
-	_refresh() {
-		const REFRESH_RATE = 300;
-
-		// this.player = this.players.pick();
-		this._setText();
-		this._setIcon();
-		this._removeTimeout();
-
-		this._timeout = Mainloop.timeout_add(REFRESH_RATE, this._refresh.bind(this));
-		return true;
-	}
-
-	_setIcon(){
-	// 	const ICON_PLACE = this.settings.get_string('show-icon');
-	// 	const PLACEHOLDER = this.settings.get_string('button-placeholder');
-
-	// 	if(this.icon){
-	// 		this.box.remove_child(this.icon);
-	// 		this.icon = null;
-	// 	}
-
-	// 	if(!ICON_PLACE || !this.player || this.label.get_text() == "" || this.label.get_text() == PLACEHOLDER)
-	// 		return
-
-	// 	this.icon = this.player.icon
-
-	// 	if (this.icon != null | undefined){
-	// 		if (ICON_PLACE == "right")
-	// 			this.box.add_child(this.icon);
-	// 		else if (ICON_PLACE == "left")
-	// 			this.box.insert_child_at_index(this.icon,0);
-	// 	}
-	}
-
-	_setText() {
-	// 	try{
-	// 		if(this.player == null || undefined)
-	// 			this.label.set_text("")
-	// 		else
-	// 			this.label.set_text(buildLabel(this.players));
-	// 	}
-	// 	catch(err){
-	// 		log("FastUserSwitch: " + err);
-	// 		this.label.set_text("");
-	// 	}
-	}
-
-	_removeTimeout() {
-		if(this._timeout) {
-			Mainloop.source_remove(this._timeout);
-			this._timeout = null;
+		this._user_manager = AccountsService.UserManager.get_default();
+		if (!this._user_manager.is_loaded) {
+			this._user_manager_loaded_id = 	this._user_manager.connect('notify::is-loaded',this._onUserManagerLoaded.bind(this));
+		} 
+		else {
+			this._onUserManagerLoaded();
 		}
+
+		Main.panel.addToStatusArea('PanelUserSwitch',this,0,'right'); //position,panel_side
+	}
+
+	_updateMenu() {
+		this.menu.removeAll();
+		this._items = [];
+		let user_names = new Array();
+		this._users.forEach((item) => {
+		  if (item.get_user_name() != GLib.get_user_name() && item.is_logged_in()) {
+			this._items[item.get_real_name()] = item;
+			user_names.push(item.get_real_name());
+			log(Date().substring(16,24)+' fastuserswitch/src/extension.js: '+item.get_real_name()+' added');
+		}
+		else {
+			log(Date().substring(16,24)+' fastuserswitch/src/extension.js: '+item.get_real_name()+' excluded');
+		}
+		});
+	
+		user_names.forEach((item) => {
+			// let menu_item = new UserMenuItem(this._user_manager, this._items[item]);
+			let menu_item = new PopupMenu.PopupMenuItem(this._items[item].get_real_name());
+
+			menu_item.connect('activate', () => {
+				if (this._items[item].is_logged_in()) {
+					log(Date().substring(16,24)+' fastuserswitch/src/extension.js: '+this._items[item].get_real_name()+' logged in, switching now');
+					let gdmClient = new Gdm.Client();
+					menu_item._authPrompt = new AuthPrompt.AuthPrompt(gdmClient, AuthPrompt.AuthPromptMode.UNLOCK_ONLY);
+					menu_item._authPrompt.begin({ userName: this._items[item].get_user_name() });
+				} 
+				else {
+					log(Date().substring(16,24)+' fastuserswitch/src/extension.js: '+this._items[item].get_real_name()+' not logged in, drop back to GDM login screen');
+					// In case something is wrong, drop back to GDM login screen
+					Gdm.goto_login_session_sync(null);
+				}
+			});
+			
+			this.menu.addMenuItem(menu_item);
+		});
+		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+		this._switch_user_item = new PopupMenu.PopupMenuItem(_("Login Screen"));
+		this._switch_user_item.connect('activate', this._onSwitchUserActivate.bind(this));
+		this.menu.addMenuItem(this._switch_user_item);
+
+		// this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+		// this.menu.addAction(_('Settings'), () => ExtensionUtils.openPrefs());
+	}
+
+	_onSwitchUserActivate() {
+		Gdm.goto_login_session_sync(null);
+	}
+
+	_onUserManagerLoaded() {
+		this._users = this._user_manager.list_users();
+		this._updateMenu();
+		this._user_manager.connect('user-is-logged-in-changed',this._updateMenu.bind(this));
 	}
 
 	_disable(){
-		// if(this.icon)
-		// 	this.box.remove_child(this.icon);
+		if(this.icon)
+			this.box.remove_child(this.icon);
 
-		this.box.remove_child(this.label);
+		if (this.label)
+			this.box.remove_child(this.label);
+
 		this.remove_child(this.box);
-		this._removeTimeout();
-
-		if (this._repositionTimeout){
-			GLib.Source.remove(this._repositionTimeout);
-			this._repositionTimeout = null;
-		}
 	}
 });
 
